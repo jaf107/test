b6dbed79b887ffe0cdf7bba31631c065142f9f62
jaf107 <48786629+jaf107@users.noreply.github.com>
2019-03-21 14:02:26 +0600

    Add files via upload

---
+++ Freq_determiner.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n,i,j,temp,count;
+    char inputA[]="I am Mahin";
+    char outputA[12][2];
+    //gets(inputA);
+    n=strlen(inputA);
+
+    for(i=0;i<n;i++)
+    {
+        outputA[i][0]=inputA[i];
+    }
+
+    for(i=0;i<n;i++)
+    {
+        outputA[i][0]=count=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(inputA[i]==inputA[j])
+            {
+                outputA[i][1] = count++;
+                outputA[j][0]= 69 ;
+                //temp=1;
+                break;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<2;j++)
+        {   if(outputA != 69)
+            {
+                printf("%c is %d times\n", outputA[i][0],outputA[i][j]);
+            }
+        }
+    }
+
+    return 0;
+}
